a561e3939289f61c340d40f92a8edb57571f9247
Hirschgott <yannickleinmueller@web.de>
2024-11-08 15:11:32 +0100

    testing.html erstellt, header eingefügt

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors:{
+        "wm-black": "#201e1e",
+        "wm-brown": "#b07141",
+        "wm-beige": "#e7dfc8",
+        "wm-darkbrown": "#50211d",
+        "wm-blue": "#00CED1",
+      }
+    },
+    fontFamily: {
+      Noto: ["Noto, sans-serif"],
+    },
+    container: {
+      center:true,
+    }
+  },
+  plugins: [],
+}
+
