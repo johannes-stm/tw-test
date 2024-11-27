36f034fc88c2761779b71eff281591f0dc43711d
Hirschgott <yannickleinmueller@web.de>
2024-11-27 22:47:17 +0100

    tw-config angepasst .container, homepage schöner, kleinigkeiten, faq hinzugefügt

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,22 @@ module.exports = {
       colors:{
         "wm-black": "#201e1e",
         "wm-brown": "#b07141",
-        "wm-beige": "#e7dfc8",
+        "wm-beige": "#e7dfc8 ",
         "wm-darkbrown": "#50211d",
         "wm-blue": "#00CED1",
-      }
+      },
+      container: {
+        center: true, // Optional: Container wird zentriert
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+          '2xl': '1300px', // Breakpoint 1280px wird auf 1300px geändert
+        },
+      },
     },
+    
     fontFamily: {
       sans: ['ui-sans-serif', 'system-ui'],
       serif: ['ui-serif', 'Georgia'],
