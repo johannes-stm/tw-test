932a7305ad114256ac7b58e4a40474e3b904b6f6
johannes-stm <johannesstegmaier9@gmail.com>
2024-12-03 20:54:03 +0100

    add scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [],
   theme: {
     extend: {
-      colors:{
+      colors: {
         "wm-black": "#201e1e",
         "wm-brown": "#b07141",
         "wm-beige": "#f7ede2",
@@ -12,26 +12,31 @@ module.exports = {
         "wm-lightgray": "#4b5563",
         "wm-darkgray": "#1f2937",
       },
-container: {
-  center: true,
-  padding: {
-    DEFAULT: '1rem', 
-    sm: '1.5rem', 
-  },
-  screens: {
-    sm: '640px',
-    md: '768px',
-    lg: '1024px',
-    xl: '1280px',
-    '2xl': '1300px',
-  },
-},
-
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1026px', 
+        xl: '1280px',
+        '2xl': '1300px',
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '1.5rem',
+        },
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1026px',
+          xl: '1280px',
+          '2xl': '1300px',
+        },
+      },
       scale: {
         '103': '1.03',
       },
     },
-    
     fontFamily: {
       sans: ['ui-sans-serif', 'system-ui'],
       serif: ['ui-serif', 'Georgia'],
